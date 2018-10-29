c55f3aec61ab93b061666ee0651dae7d85f364a7
farooqam <farooq_am@hotmail.com>
2018-10-28 22:38:24 -0700

    Initial commit.

---
+++ Globomantics/BandAgent/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Data.Edm.Csdl;
+
+namespace Globomantics.BandAgent
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var connectionString = "HostName=ps-iothub-fm.azure-devices.net;DeviceId=device1;SharedAccessKey=ZJNtwonsWec64h0N0sXaW4jaml5CJ5JVCZMIO8S9erQ=";
+            var device = DeviceClient.CreateFromConnectionString(connectionString);
+
+            Console.WriteLine("Initializing...");
+
+            await device.OpenAsync();
+
+            Console.WriteLine("Device is connected.");
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
